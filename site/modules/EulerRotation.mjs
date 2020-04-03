d19bae3acc76c82049a2f9ec18abb92aee41da05
Willee <wilson.lee@cae.com>
2020-04-02 22:21:30 -0400

    Extracted Planet Model

---
+++ site/modules/EulerRotation.mjs
@@ -0,0 +1,55 @@
+const gDegToRad = Math.PI/180;
+const gRadToDeg = 180/Math.PI;
+
+export function rotateAxisCW(iAngle, iAxis, iVector) {
+  var wTempVector = [iVector[0], iVector[1], iVector[2]];
+
+  var wAxis1 = (iAxis + 1) % 3;
+  var wAxis2 = (iAxis + 2) % 3;
+  var wCAng = Math.cos(iAngle * gDegToRad);
+  var wSAng = Math.sin(iAngle * gDegToRad);
+  wTempVector[wAxis1] = iVector[wAxis1] * wCAng + iVector[wAxis2] * wSAng
+  wTempVector[wAxis2] = -iVector[wAxis1] * wSAng + iVector[wAxis2] * wCAng
+
+  return wTempVector;
+}
+
+export function rotateAxisCCW(iAngle, iAxis, iVector) {
+  var wTempVector = [iVector[0], iVector[1], iVector[2]];
+
+  var wAxis1 = (iAxis + 1) % 3;
+  var wAxis2 = (iAxis + 2) % 3;
+  var wCAng = Math.cos(iAngle * gDegToRad);
+  var wSAng = Math.sin(iAngle * gDegToRad);
+  wTempVector[wAxis1] = iVector[wAxis1] * wCAng - iVector[wAxis2] * wSAng
+  wTempVector[wAxis2] = iVector[wAxis1] * wSAng + iVector[wAxis2] * wCAng
+
+  return wTempVector;
+}
+
+export function rotateNEDToBody(iRoll, iPitch, iYaw, iNEDVector) {
+  var wBodyVector = [iNEDVector[0], iNEDVector[1], iNEDVector[2]];
+  wBodyVector = rotateAxisCW(iYaw, 2, wBodyVector);
+  wBodyVector = rotateAxisCW(iPitch, 1, wBodyVector);
+  wBodyVector = rotateAxisCW(iRoll, 0, wBodyVector);
+
+  return wBodyVector;
+}
+
+export function rotateBodyToNED(iRoll, iPitch, iYaw, iBodyVector) {
+  var wNEDVector = [iBodyVector[0], iBodyVector[1], iBodyVector[2]];
+  wNEDVector = rotateAxisCW(-iRoll, 0, wNEDVector);
+  wNEDVector = rotateAxisCW(-iPitch, 1, wNEDVector);
+  wNEDVector = rotateAxisCW(-iYaw, 2, wNEDVector);
+
+  return wNEDVector;
+
+}
+
+export default {
+  rotateAxisCW: rotateAxisCW,
+  rotateAxisCCW: rotateAxisCCW,
+  rotateNEDToBody: rotateNEDToBody,
+  rotateBodyToNED: rotateBodyToNED
+}
+
---
+++ site/modules/PlanetModel.mjs
@@ -0,0 +1,78 @@
+import EulerRotation from "./EulerRotation.mjs";
+
+const gDegToRad = Math.PI/180;
+const gRadToDeg = 180/Math.PI;
+const gPlanetRadius = 6371000; //m
+
+
+export function convertGeoCoordinateToGeoCentric (iLatitude, iLongitude, iAltitude ) {
+      
+  var wLatitude = iLatitude*gDegToRad;
+  var wLongitude = iLongitude*gDegToRad;
+
+  var wRadius = iAltitude + gPlanetRadius;
+  var wEquatorRadius = wRadius*Math.cos(wLatitude);
+  
+  return {
+    x : wEquatorRadius*Math.sin(wLongitude),
+    y : wRadius*Math.sin(wLatitude),
+    z : wEquatorRadius*Math.cos(wLongitude)
+  }
+}
+
+
+export function addNEDVectorToGeoLocation (iLatitude, iLongitude , iAltitude, iNEDVector) {
+      
+  var wLatitude = iLatitude*gDegToRad;
+  var wLongitude = iLongitude*gDegToRad;
+
+  wLatitude += iNEDVector[0]/gPlanetRadius;
+
+  var wHalfPi = Math.PI/2;
+  var w2Pi = Math.PI*2;
+  
+  while (wLatitude > wHalfPi) {
+    wLatitude = Math.PI - wLatitude;
+    wLongitude += Math.PI;
+  }
+  
+  while (wLatitude < -wHalfPi) {
+    wLatitude = -Math.PI - wLatitude;
+    wLongitude += Math.PI;
+  }
+  
+  var wLatitudeRadius = Math.cos(wLatitude)*gPlanetRadius;
+
+  wLongitude += iNEDVector[1]/wLatitudeRadius;
+
+  while (wLongitude > Math.PI) {
+    wLongitude -= w2Pi;
+  }
+  while (wLongitude < -Math.PI) {
+    wLongitude += w2Pi;
+  }
+
+  var wAltitude = iAltitude - iNEDVector[2];
+  
+  return {
+    latitude : wLatitude*gRadToDeg,
+    longitude : wLongitude*gRadToDeg,
+    altitude : wAltitude
+  }
+}
+
+
+export function addBodyVectorToGeoLocation (iLatitude, iLongitude , iAltitude, iRoll, iPitch, iYaw, iBodyVector) {
+  
+  var wNEDVector = EulerRotation.rotateBodyToNED( iRoll, iPitch, iYaw, iBodyVector);
+  return addNEDVectorToGeoLocation(iLatitude, iLongitude , iAltitude , wNEDVector);
+}
+
+
+export default {
+  Radius : gPlanetRadius,
+  convertGeoCoordinateToGeoCentric : convertGeoCoordinateToGeoCentric,
+  addNEDVectorToGeoLocation : addNEDVectorToGeoLocation,
+  addBodyVectorToGeoLocation : addBodyVectorToGeoLocation
+}
+
