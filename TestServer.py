48110effa894fe53f099d99b1f5c22c8cd64511c
Willee <wilson.lee@cae.com>
2020-04-02 20:58:33 -0400

    initial submit

---
+++ TestServer.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+
+@app.route('/')
+def send_index():
+    return send_from_directory('./', 'index.html')
+    
+@app.route('/site/<path:path>')
+def send_site(path):
+    return send_from_directory('./site', path)
+
+def main():
+
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+if __name__ == '__main__':
+    main()
